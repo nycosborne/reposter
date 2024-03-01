84f23e0f364be92faa0504b9508c1b9c5fed71ac
nycosborne <nycosborne@gmail.com>
2024-03-01 09:39:54 -0500

    added .env file and vars

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ import requests.auth
 import os
 from dotenv import load_dotenv
 
-
 load_dotenv()
+
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+
+USERNAME = os.getenv('USERNAME')
+PASSWORD = os.getenv('PASSWORD')
 
 event = {
     'post_id': '123',
