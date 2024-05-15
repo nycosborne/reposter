fa739679f3c58310732093f6817995c341ad06a4
nycosborne <nycosborne@gmail.com>
2024-05-15 11:19:55 -0400

    User admin config 2

--- app/core/admin.py
+++ app/core/admin.py
@@ -12,6 +12,7 @@ class UserAdmin(BaseUserAdmin):
     """User admin configuration."""
     ordering = ['id']
     list_display = ['email', 'name']
+    # Add a new field to the list display for users
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         ('Personal Info', {'fields': ('name',)}),
@@ -22,6 +23,21 @@ class UserAdmin(BaseUserAdmin):
         ('Important dates', {'fields': ('last_login',)})
     )
     readonly_fields = ['last_login']
+    # Add a new fieldset for creating a new user
+    add_fieldsets = (
+        (
+            None,
+            {
+                'classes': ('wide',),  # Custom CSS classes
+                'fields': ('email',
+                           'password1',
+                           'password2',
+                           'name',
+                           'is_staff',
+                           'is_superuser')
+            }
+        ),
+    )
 
 
 admin.site.register(models.User, UserAdmin)
