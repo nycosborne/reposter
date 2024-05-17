90e0a04bb21210ae5a4dacb17906d013c9d7f9e8
nycosborne <nycosborne@gmail.com>
2024-05-17 14:12:16 -0400

    Added serializer and view for token

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -8,6 +8,7 @@ from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
+ME_URL = reverse('user:me')
 
 
 def create_user(**params):
@@ -107,3 +108,57 @@ class PublicUserApiTests(TestCase):
         response = self.client.post(TOKEN_URL, payload)
         self.assertNotIn('token', response.data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_retrieve_user_unauthorized(self):
+        """Test that authentication is required for users."""
+        response = self.client.get(ME_URL)
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateUserApiTests(TestCase):
+    """Test API requests that require authentication."""
+
+    def setUp(self):
+        """Setup function for the user API test case."""
+        """Create a user and authenticate the client."""
+        self.user = create_user(
+            email='test@example.com',
+            password='password123',
+            name='Test Name',
+        )
+
+        self.client = APIClient()
+        # todo: JWT update required (maybe)
+        self.client.force_authenticate(user=self.user)
+
+    def test_retrieve_profile_success(self):
+        """Test retrieving profile for logged in used."""
+        response = self.client.get(ME_URL)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, {
+            'name': self.user.name,
+            'email': self.user.email
+        })
+
+    def test_post_me_not_allowed(self):
+        """Test that POST is not allowed on the ME URL."""
+        response = self.client.post(ME_URL, {})
+
+        self.assertEqual(response.status_code,
+                         status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_user_profile(self):
+        """Test updating the user profile for authenticated user."""
+        payload = {
+            'name': 'New Name',
+            'password': 'newpassword123'
+        }
+
+        response = self.client.patch(ME_URL, payload)
+
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.name, payload['name'])
+        self.assertTrue(self.user.check_password(payload['password']))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
--- app/user/views.py
+++ app/user/views.py
@@ -2,7 +2,7 @@
 This file defines the views for the user app.
 """
 
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 # todo: JWT update required
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
@@ -22,3 +22,17 @@ class CreateTokenView(ObtainAuthToken):  # todo: JWT update required
     """Create a new auth token for the user."""
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+# RetrieveUpdateAPIView is used for get and patch/put request
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user."""
+    serializer_class = UserSerializers
+    # todo: JWT update required
+    authentication_classes = [authentication.TokenAuthentication]
+    # todo: JWT update required
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """Retrieve and return authenticated user."""
+        return self.request.user
