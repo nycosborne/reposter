aa40387cbedeeb8a71e209449afdb12508945c19
nycosborne <nycosborne@gmail.com>
2024-05-21 12:56:10 -0400

    Added urls and fixed serializer

---
+++ app/post/serializers.py
@@ -0,0 +1,30 @@
+"""
+Serializers for the api end points of the post
+"""
+
+from rest_framework import serializers
+
+from core.models import Post
+
+
+class PostSerializer(serializers.Serializer):
+    """Serializers for the post object."""
+
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'content', 'description', 'link']
+        read_only_fields = ['id']
+
+    # def create(self, validated_data):
+    #     """Create a new post and return it."""
+    #     return Post.objects.create(**validated_data)
+    #
+    # def update(self, instance, validated_data):
+    #     """Update a post and return it."""
+    #     instance.title = validated_data.get('title', instance.title)
+    #     instance.content = validated_data.get('content', instance.content)
+    #     instance.description = validated_data.get('description',
+    #     instance.description)
+    #     instance.link = validated_data.get('link', instance.link)
+    #     instance.save()
+    #     return instance
--- app/post/views.py
+++ app/post/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+""" Views for the post app. """
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from core.models import Post
+from post import serializers
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    """manage posts API."""
+    serializer_class = serializers.PostSerializer
+    queryset = Post.objects.all()  # Limits the queryset
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Return objects for the current authenticated user only."""
+        return self.queryset.filter(user=self.request.user).order_by('-id')
+
+    # def perform_create(self, serializer):
+    #     """Create a new post."""
+    #     serializer.save(user=self.request.user)
+    #
+    # def get_serializer_class(self):
+    #     """Return appropriate serializer class."""
