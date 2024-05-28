a9ff3bdc467eeef5c495a77050907bc9cc1888a1
nycosborne <nycosborne@gmail.com>
2024-05-28 12:43:29 -0400

    configured app to used statis files

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,6 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+
+from django.conf.urls.static import static
+from django.conf import settings
+
 from drf_spectacular.views import (
     SpectacularAPIView,
     # SpectacularRedocView, # noqa
@@ -28,3 +32,13 @@ urlpatterns = [
     path('api/user/', include('user.urls')),
     path('api/post/', include('post.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
+    # urlpatterns += static(
+    #     settings.STATIC_URL,
+    #     document_root=settings.STATIC_ROOT
+    # )
