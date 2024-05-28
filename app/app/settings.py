fbc07faa08d3aa2b24e33c9edc7234f123ba2bab
nycosborne <nycosborne@gmail.com>
2024-05-28 18:21:07 -0400

    new checks file

--- app/app/settings.py
+++ app/app/settings.py
@@ -123,9 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/static/'
-MEDIA_URL = '/static/media/'
+MEDIA_URL = '/media/'
 
-MEDIA_ROOT = '/vol/web/media'
+MEDIA_ROOT = '/vol/web/media/uploads'
 STATIC_ROOT = '/vol/web/static'
 
 # Default primary key field type
