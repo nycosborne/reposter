9c20d43e629411a71fe8167751230a2d93ab49ad
nycosborne <nycosborne@gmail.com>
2024-05-28 16:17:07 -0400

    working!!!!

--- app/core/models.py
+++ app/core/models.py
@@ -14,8 +14,8 @@ from django.contrib.auth.models import (
 
 def post_image_file_path(instance, filename):
     """Generate file path for new post image"""
-    ext = filename.split('.')[-1]
-    filename = f'{uuid.uuid4()}.{ext}'
+    ext = os.path.splitext(filename)[1]
+    filename = f'{uuid.uuid4()}{ext}'
 
     return os.path.join('uploads/post/', filename)
 
--- app/post/serializers.py
+++ app/post/serializers.py
@@ -63,3 +63,15 @@ class PostDetailSerializer(PostSerializer):
 
     class Meta(PostSerializer.Meta):
         fields = PostSerializer.Meta.fields + ['description']
+
+
+class PostImageSerializer(serializers.ModelSerializer):
+    """Serializer for uploading images to posts"""
+
+    class Meta:
+        model = Post
+        fields = ['id', 'image']
+        read_only_fields = ['id']
+        extra_kwargs = {
+            'image': {'required': True}
+        }
