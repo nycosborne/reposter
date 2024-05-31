9fff07f5aaaecde7ff5cdb4bc14785190b9c0ffe
nycosborne <nycosborne@gmail.com>
2024-05-31 15:56:16 -0400

    Passed local deploy test

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,4 +6,5 @@ python manage.py wait_for_db
 python manage.py collectstatic --noinput
 python manage.py migrate
 
-uwsgi --socket :9000 --worker 4 --master --enable-threads --module app.wsgi
+uwsgi --socket :9000 --processes 5 --master --enable-threads --module app.wsgi
+#uwsgi --http :9000 --module app.wsgi:application --master --processes 5
