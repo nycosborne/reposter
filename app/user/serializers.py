98f62427abf9c6df1f88e4cb2d0a6f12b3716cc1
nycosborne <nycosborne@gmail.com>
2024-06-20 16:48:21 -0400

    Updated added secAccount fields to the post model, updated serializers.py

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -16,7 +16,7 @@ class UserSerializers(serializers.ModelSerializer):
 
     class Meta:
         model = get_user_model()
-        fields = ('email', 'password', 'first_name', 'last_name')
+        fields = ('email', 'password', 'first_name', 'last_name', 'soc_accounts')
         extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
 
     def create(self, validated_data):
