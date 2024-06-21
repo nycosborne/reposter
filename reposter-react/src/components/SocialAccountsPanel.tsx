c6310fbd8d517deb650293f77210fb4abadb72fd
nycosborne <nycosborne@gmail.com>
2024-06-21 15:47:54 -0400

    refacted SocialAccountsPanel.tsx

--- reposter-react/src/components/SocialAccountsPanel.tsx
+++ reposter-react/src/components/SocialAccountsPanel.tsx
@@ -4,12 +4,11 @@ import useAppContext from "../context/UseAppContext.tsx";
 import {faLinkedin, faRedditAlien} from '@fortawesome/free-brands-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faLinkSlash} from "@fortawesome/free-solid-svg-icons";
-import { useNavigate } from "react-router-dom";
-
+import {IconProp} from '@fortawesome/fontawesome-svg-core';
+import {useNavigate} from "react-router-dom";
 
 const SocialAccountsCard = (): React.JSX.Element => {
     const {user} = useAppContext();
-
     const navigate = useNavigate();
 
     if (!user) {
@@ -19,9 +18,24 @@ const SocialAccountsCard = (): React.JSX.Element => {
 
     const handleRedditLink = () => {
         navigate("/new-route");
-        // console.log("Reddit Link Clicked")
     }
 
+    const renderSocialAccount = (isLinked: boolean, icon: IconProp, color: string, unlinkedText: string, handleLink: () => void) => (
+        isLinked ?
+            <ListGroup.Item>
+                <FontAwesomeIcon icon={icon} size="2x" color={color}/>
+            </ListGroup.Item>
+            :
+            <ListGroup.Item action onClick={handleLink}>
+                <Row>
+                    <FontAwesomeIcon icon={icon} size="2x" color="gray"/>
+                    <div className="link-icon-container">
+                        <FontAwesomeIcon icon={faLinkSlash} size="sm" color="black"/>
+                    </div>
+                    <p>{unlinkedText}</p>
+                </Row>
+            </ListGroup.Item>
+    );
 
     return (
         <Container>
@@ -30,44 +44,13 @@ const SocialAccountsCard = (): React.JSX.Element => {
                     <Card.Title as="h4" className="header">
                         Social Accounts Status
                     </Card.Title>
-                    {user &&
-                        <ListGroup>
-                            {user && user.reddit ?
-                                <ListGroup.Item>
-                                    <FontAwesomeIcon icon={faRedditAlien} size="2x" color="#FF5700"/>
-                                </ListGroup.Item>
-                                :
-                                <ListGroup.Item action onClick={handleRedditLink}>
-                                    <Row>
-                                        <FontAwesomeIcon icon={faRedditAlien} size="2x" color="gray"/>
-                                        <div className={"link-icon-container"}>
-                                            <FontAwesomeIcon icon={faLinkSlash} size="sm" color="black"/>
-                                        </div>
-                                        <p>Reddit Unlinked</p>
-                                    </Row>
-                                </ListGroup.Item>
-                            }
-                            {user && user.linkedin ?
-                                <ListGroup.Item>
-                                    <FontAwesomeIcon icon={faLinkedin} size="2x" color="#0072b1"/>
-                                </ListGroup.Item>
-                                :
-                                <ListGroup.Item>
-                                    <Row>
-                                        <FontAwesomeIcon icon={faLinkedin} size="2x" color="gray"/>
-                                        <div className={"link-icon-container"}>
-                                            <FontAwesomeIcon icon={faLinkSlash} size="sm" color="black"/>
-                                        </div>
-                                        <p>LinkedIn Unlinked</p>
-                                    </Row>
-                                </ListGroup.Item>
-                            }
-                        </ListGroup>
-                    }
+                    <ListGroup>
+                        {renderSocialAccount(user.reddit, faRedditAlien, "#FF5700", "Reddit Unlinked", handleRedditLink)}
+                        {renderSocialAccount(user.linkedin, faLinkedin, "#0072b1", "LinkedIn Unlinked", handleRedditLink)}
+                    </ListGroup>
                 </Card.Body>
             </Card>
         </Container>
-
     );
 };
 
