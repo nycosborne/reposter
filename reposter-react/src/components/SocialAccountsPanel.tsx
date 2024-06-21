600f9add84dad38f002281651377eb105166a09e
nycosborne <nycosborne@gmail.com>
2024-06-21 10:08:34 -0400

    more user interface work

--- reposter-react/src/components/SocialAccountsPanel.tsx
+++ reposter-react/src/components/SocialAccountsPanel.tsx
@@ -1,9 +1,37 @@
+import React, { useEffect } from 'react';
 import { Card, ListGroup, Container } from 'react-bootstrap';
 import useAppContext from "../context/UseAppContext.tsx";
 
 const SocialAccountsCard = (): React.JSX.Element => {
-    const {user} = useAppContext();
-console.log('user', user.reddit)
+    const {user, setUser} = useAppContext();
+
+
+        if (!user) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <Container>
+            <Card>
+                <Card.Body>
+                    <Card.Title as="h4" className="header">
+                        Social Accounts
+                    </Card.Title>
+                    {user.linkedin &&
+                        <ListGroup>
+                            <ListGroup.Item>
+                                LinkedIn
+                            </ListGroup.Item>
+                        </ListGroup>
+                    }
+                </Card.Body>
+            </Card>
+        </Container>
+    );
+};
+
+export default SocialAccountsCard;
+
     return (
         <Container>
             <Card>
@@ -12,16 +40,16 @@ console.log('user', user.reddit)
                         Social Accounts
                     </Card.Title>
                     <ListGroup>
-                            {/*{posts.map(item => (*/}
-                            {/*    <ListGroup.Item key={item.id}>*/}
-                            {/*    <h6>{item.title}</h6>*/}
-                            {/*    {location.pathname === '/dashboard' && <p>desc: {item.description}</p>}*/}
-                            {/*    {location.pathname === '/posts' && <p>title: {item.content}</p>}*/}
-                            {/*    {item.tags.map(tag => (*/}
-                            {/*        <span key={tag.id}>tags: {tag.name}</span>*/}
-                            {/*    ))}*/}
-                            {/*</ListGroup.Item>*/}
-                            {/*))}*/}
+                            {posts.map(item => (
+                                <ListGroup.Item key={item.id}>
+                                <h6>{item.title}</h6>
+                                {location.pathname === '/dashboard' && <p>desc: {item.description}</p>}
+                                {location.pathname === '/posts' && <p>title: {item.content}</p>}
+                                {item.tags.map(tag => (
+                                    <span key={tag.id}>tags: {tag.name}</span>
+                                ))}
+                            </ListGroup.Item>
+                            ))}
                         </ListGroup>
                 </Card.Body>
             </Card>
