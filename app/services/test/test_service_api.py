4f2702ba67a45c0569b4c41ee04eceecfac8ec02
nycosborne <nycosborne@gmail.com>
2024-06-24 14:27:40 -0400

    Adding test file

--- app/services/test/test_service_api.py
+++ app/services/test/test_service_api.py
@@ -7,3 +7,25 @@ from rest_framework.test import APIClient
 
 import tempfile
 import os
+from django.contrib.auth import get_user_model
+from core.models import SocialAccounts
+
+SERVICE_URL = reverse('services:services-list')
+
+
+def detail_url(service_id):
+    """Return service detail URL"""
+    return reverse('service:service_id-detail', args=[service_id])
+
+
+class PublicServiceApiTests(TestCase):
+    """Test the publicly available service API"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_login_required(self):
+        """Test that login is required for retrieving service"""
+        res = self.client.get(SERVICE_URL)
+        print(res)
+        # self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
