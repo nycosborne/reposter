2ec1eb137c02aec449476a6c58b2a99fe3bb306a
nycosborne <nycosborne@gmail.com>
2024-06-27 15:17:09 -0400

    lint

--- app/services/views.py
+++ app/services/views.py
@@ -7,7 +7,6 @@
 #     OpenApiTypes,
 # )
 from rest_framework import viewsets
-
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.permissions import IsAdminUser
