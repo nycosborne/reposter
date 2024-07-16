56c3ca8869de2b26f89ea89af99e1d5a27e2d777
nycosborne <nycosborne@gmail.com>
2024-07-16 16:24:47 -0400

    abstracted the linkedin api call

--- app/services/linkedinAPI.py
+++ app/services/linkedinAPI.py
@@ -12,49 +12,55 @@ class LinkedInAPI:
         self.linkedin_redirect_uri = os.getenv('LINKEDIN_REDIRECT_URI')
         self.user = user
         self.request = request
-        self.access_token = (self.user.
-                             usersocialaccountssettings_set
-                             .filter(name='linkedin')
-                             .order_by('-created_at').first())
         self.sub = (self.user.
                     linkedinuserinfo_set.order_by('-created_at').
                     first())
 
-    def post_to_linkedin(self, data, post_id):
-        print(f"Posting to LinkedIn: {data}")
-        access_token = (self.user.
-                        usersocialaccountssettings_set.filter(name='linkedin').
-                        order_by('-created_at').first().access_token)
+    def linkedin_api_request(self, method, endpoint, content=None, headers=None):
+        access_token = self.user.usersocialaccountssettings_set.filter(name='linkedin').order_by(
+            '-created_at').first().access_token
         sub = (self.user.
                linkedinuserinfo_set.order_by('-created_at').
                first().sub)
+        url = f'https://api.linkedin.com/v2/{endpoint}'
 
-        headers = {
-            'Content-Type': 'application/json',
-            'Authorization': f'Bearer {access_token}'
-        }
-
-        payload = {
-            "author": f"urn:li:person:{sub}",
-            "lifecycleState": "PUBLISHED",
-            "specificContent": {
-                "com.linkedin.ugc.ShareContent": {
-                    "shareCommentary": {
-                        "text": f"{data['content']}"
-                    },
-                    "shareMediaCategory": "NONE"
-                }
-            },
-            "visibility": {
-                "com.linkedin.ugc.MemberNetworkVisibility": "PUBLIC"
+        if not headers:
+            headers = {
+                'Content-Type': 'application/json',
+                'Authorization': f'Bearer {access_token}'
             }
-        }
 
-        response = requests.post(
-            'https://api.linkedin.com/v2/ugcPosts',
-            headers=headers, json=payload
-        )
+        payload = None
 
+        if endpoint == 'userinfo':
+            payload = None
+
+        # test post payload
+        if endpoint == 'ugcPosts':
+            payload = {
+                "author": f"urn:li:person:{sub}",
+                "lifecycleState": "PUBLISHED",
+                "specificContent": {
+                    "com.linkedin.ugc.ShareContent": {
+                        "shareCommentary": {
+                            "text": f"{content}"
+                        },
+                        "shareMediaCategory": "NONE"
+                    }
+                },
+                "visibility": {
+                    "com.linkedin.ugc.MemberNetworkVisibility": "PUBLIC"
+                }
+            }
+
+        response = requests.request(method, url, headers=headers, json=payload)
+
+        return response
+
+    def post_to_linkedin(self, data, post_id):
+        print(f"Posting to LinkedIn: {data}")
+
+        response = self.linkedin_api_request('POST', 'ugcPosts', data['content'])
         if response.status_code == 201:
             print("Post shared successfully.")
             post = self.user.post_set.get(id=post_id)
@@ -70,18 +76,19 @@ class LinkedInAPI:
                   f" Response: {response.text}")
             return False
 
-    def _get_user_info(self, access_token):
+    def _get_user_info(self):
         # TODO: need to refactor this
         # this class should not be rependent on services servicesSerializers
         from services import serializers as servicesSerializers
 
-        headers = {
-            'Authorization': f'Bearer {access_token}'
-        }
-        response = requests.get(
-            'https://api.linkedin.com/v2/userinfo',
-            headers=headers
-        )
+        # headers = {
+        #     'Authorization': f'Bearer {access_token}'
+        # }
+        # response = requests.get(
+        #     'https://api.linkedin.com/v2/userinfo',
+        #     headers=headers
+        # )
+        response = self.linkedin_api_request('GET', 'userinfo')
         print(f"Response: {response.text}")
 
         if response.status_code == 200:
@@ -128,10 +135,12 @@ class LinkedInAPI:
             'redirect_uri': self.linkedin_redirect_uri
         }
 
-        response = requests.post(
-            'https://www.linkedin.com/oauth/v2/accessToken',
-            headers=headers, data=data
-        )
+        # response = requests.post(
+        #     'https://www.linkedin.com/oauth/v2/accessToken',
+        #     headers=headers, data=data
+        # )
+
+        response = self.linkedin_api_request('POST', 'accessToken', data, headers)
 
         if response.status_code == 200:
             print("Access token obtained successfully.")
@@ -147,7 +156,8 @@ class LinkedInAPI:
                 self.user.linkedin = True
                 self.user.save()
                 serializer.save()
-                self._get_user_info(access_token_data['access_token'])
+                self._get_user_info()
+                # self._get_user_info(access_token_data['access_token'])
             else:
                 print(f"Failed to save access token data. "
                       f"Errors: {serializer.errors}")
