0189c73e6bbf2dd2c2bd49f01d08a8e7a51be75a
nycosborne <nycosborne@gmail.com>
2024-07-24 16:03:50 -0400

    test actions

--- reposter-react/src/App.tsx
+++ reposter-react/src/App.tsx
@@ -2,7 +2,6 @@ import {RouterProvider} from "react-router-dom";
 import {ContextProvider} from "./context/ContextProvider.tsx";
 import router from "./router";
 
-
 function App(): JSX.Element {  // Explicitly typing the return type
     return (
         <ContextProvider>
